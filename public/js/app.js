ee5481289d619011f6a6b3ef80569b77c788725f
Fisher Ramsey <fishram@unc.edu>
2024-05-19 00:20:46 -0400

    mvc glitched

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,11 @@
-import InventoryModel from '../../src/models/inventoryModel.js';
-import InventoryView from '../../src/views/inventoryView.js';
-import InventoryController from '../../src/controllers/inventoryController.js';
+import InventoryModel from './models/inventoryModel.js';
+import InventoryView from './views/inventoryView.js';
+import InventoryController from './controllers/inventoryController.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
     const model = new InventoryModel();
     const view = new InventoryView();
     const controller = new InventoryController(model, view);
+    console.log('MVC components initialized');
 });
